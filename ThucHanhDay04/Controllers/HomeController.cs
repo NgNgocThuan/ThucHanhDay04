dd53601e9c53d42e30db6f24e97f695f82d142e6
NgNgocThuan <nguyenngocthuan18082005@gmail.com>
2025-09-18 23:38:58 +0700

    Day04_wed

---
+++ ThucHanhDay04/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ThucHanhDay04.Models;
+
+namespace ThucHanhDay04.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        Product products = new Product();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About()
+        {
+            var model = products.GetListProduct();
+            return View(model);
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
---
+++ ThucHanhDay04/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿namespace ThucHanhDay04.Models
+{
+    public class Product
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+
+        public string Soluong { get; set; }
+        public string Danhmuc { get; set; }
+
+        public List<Product> GetListProduct()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product
+                {
+                    ID = "01",
+                    Name = "Quần sort nam",
+                    Price = "129000 đ",
+                    Image = "/Image/Quan.jpg",
+                    Soluong = "20",
+                },
+                new Product
+                {
+                    ID = "02",
+                    Name = "Áo sơ mi nam cotton",
+                    Price = "119000đ",
+                    Image = "/Image/Ao.jpg",
+                    Soluong = "20",
+                },
+                new Product
+                {
+                    ID = "03",
+                    Name = "Giày thể thao nam GTT23.4",
+                    Price = "500000đ",
+                    Image = "/Image/Giay.jpg",
+                    Soluong = "20",
+                }
+
+            };
+
+
+            return products;
+        }
+    }
+}
